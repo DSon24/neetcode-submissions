912a2b6cb279ecb9aa636a9ed20adbae297eb227
DSon24 <songuyen2401@gmail.com>
2026-08-30 16:09:31 -0400

    Add: two-integer-sum - submission-45

---
+++ Data Structures & Algorithms/two-integer-sum/submission-45.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr = {}
+        for i,n in enumerate(nums):
+            if target - n in arr:
+                return [arr[target - n],i]
+            else:
+                arr[n] = i
+        
